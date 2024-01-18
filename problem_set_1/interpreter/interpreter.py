fad6a8eda39cedd7263c7660c9f9a5eb8bf52a56
AlWaleed Musa <jako_249@hotmail.com>
2024-01-18 20:30:49 +0200

    tasl interprenter added

---
+++ problem_set_1/interpreter/interpreter.py
@@ -0,0 +1,14 @@
+string = input("Expression: ").strip()
+string_to_list = string.split()
+num1 = int(string_to_list[0])
+op = string_to_list[1]
+num2 = int(string_to_list[2])
+
+if op == "+":
+	print("{}".format(float(num1 + num2)))
+elif op == "-":
+	print("{}".format(float(num1 - num2)))
+elif op == "/":
+	print("{}".format(float(num1 / num2)))
+elif op == "*":
+	print("{}".format(float(num1 * num2)))
